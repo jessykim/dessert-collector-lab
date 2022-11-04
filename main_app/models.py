bbaf01f787cea8e938b3478ca2e0077283414b5b
jessikim <jessica.kim17@gmail.com>
2022-11-04 15:12:55 -0500

    create dessert model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Dessert(models.Model):
+  name = models.CharField(max_length=100)
+  category = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+
+  def __str__(self):
+    return self.name
