4d79e6d1fb0898cbb647b59aaa02b24912ae28d7
jessikim <jessica.kim17@gmail.com>
2022-11-07 15:41:18 -0600

    create new recipe model and register model to admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Dessert
+from .models import Dessert, Recipe
 
 # Register your models here.
 admin.site.register(Dessert)
+admin.site.register(Recipe)
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.urls import reverse
 
+RESTRICTIONS = (
+  ('N', 'None'),
+  ('D', 'Dairy-Free'),
+  ('G', 'Gluten-Free'),
+  ('L', 'Lactose-Free'),
+  ('N', 'Nut-Free'),
+  ('S', 'Soy-Free'),
+  ('V', 'Vegan')
+)
+
 # Create your models here.
 class Dessert(models.Model):
   name = models.CharField(max_length=100)
@@ -12,4 +22,21 @@ class Dessert(models.Model):
 
   def get_absolute_url(self):
       return reverse('desserts_detail', kwargs={'dessert_id': self.id})
-  
+
+class Recipe(models.Model):
+  name = models.CharField(max_length=50)
+  restrictions = models.CharField(
+    'Dietary Restrictions',
+    max_length=1,
+    choices=RESTRICTIONS,
+    default=RESTRICTIONS[0][0]
+  )
+  url = models.URLField(
+    'Link to Recipe',
+    max_length=200
+  )
+
+  dessert = models.ForeignKey(Dessert, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
