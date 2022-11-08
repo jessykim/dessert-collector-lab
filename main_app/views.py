8e6b3ee8289bcffb8a7d2cb19841860965089eaf
jessikim <jessica.kim17@gmail.com>
2022-11-08 16:51:29 -0600

    update dessertcreate view to assign a new dessert to the logged in user

--- main_app/views.py
+++ main_app/views.py
@@ -30,6 +30,10 @@ class DessertCreate(CreateView):
   model = Dessert
   fields = ['name', 'category', 'description']
 
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
 class DessertUpdate(UpdateView):
   model = Dessert
   fields = ['category', 'description']
