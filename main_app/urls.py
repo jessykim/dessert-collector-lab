742f074d7bd1e3be95381ecb8f64afd943168684
jessikim <jessica.kim17@gmail.com>
2022-11-08 16:37:31 -0600

    modify home template and add login form

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('desserts/', views.desserts_index, name='desserts_index'),
   path('desserts/<int:dessert_id>/', views.desserts_detail, name='desserts_detail'),
